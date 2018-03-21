909c9dafa4b3abf8fc02db0389d775b77433d6cd
Alexandra Sazhin <alexandra@sazhin.us>
2018-03-20 20:50:16 -0400

    commit 2 still problems

---
+++ app/src/main/java/com/ser210_02_asazhin/ch9_bitsandpizzas_03/OrderActivity.java
@@ -0,0 +1,14 @@
+package com.ser210_02_asazhin.ch9_bitsandpizzas_03;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
